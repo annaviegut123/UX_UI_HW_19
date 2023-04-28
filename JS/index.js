480609bda3f564515f4d351ba4dffa70bfb34458
Anna <Anna.Viegut@gmail.com>
2023-04-27 22:00:00 -0500

    javascript edits

--- JS/index.js
+++ JS/index.js
@@ -1,14 +1,7 @@
 // javascript for preloader animation
 
-let firstTime = 1;
-
 function fade() {
     $('.preloader').fadeOut("slow");
-    firstTime = 0;
 }
 
-if (firstTime === 1) {
-    setTimeout(fade, 2500);
-} else {
-    document.getElementById('.preloader').style.visibility = 'hidden';
-}
+setTimeout(fade, 2500);
